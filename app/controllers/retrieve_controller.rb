4e4c607e8e48410a964b0c5a643a2558b0c19159
AkitaOnRails <fabioakita@gmail.com>
2011-02-15 13:34:10 -0200

    Small tweak in the poll version to not render anything if there is no messages to render

--- app/controllers/retrieve_controller.rb
+++ app/controllers/retrieve_controller.rb
@@ -11,8 +11,10 @@ class RetrieveController < ApplicationController
     @last_message ||= (Time.now - 30.seconds)
     Chat.recent(@last_message).all do |messages|
       list = messages.map { |msg| { "from" => msg.name, "msg" => msg.message, "sent" => msg.sent_at.to_formatted_s(:short), "timestamp" => msg.sent_at } }
-      @last_message = messages.first.try(:sent_at) || @last_message
-      render [list.to_json, "\n"]
+      if list.size > 0
+        @last_message = messages.first.try(:sent_at) || @last_message
+        render list.to_json
+      end
       finish
     end
   end
